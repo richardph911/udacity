c4eec5c76ef5357da8de3800f4c99dafa8eaeb44
Dzung Pean <hoangtudauphong@gmail.com>
2022-02-12 21:25:01 -0600

    "create todo and todolists"

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import sys
 from sqlalchemy import MetaData, create_engine, Table, Column, ForeignKey, String, Boolean, Integer
 from sqlalchemy.orm import Session, relationship
 
+engine = create_engine('postgresql://richardph911@localhost:5432')
+session = Session(engine)
 
 def create_app():
     app = Flask(__name__)
@@ -15,10 +17,6 @@ def create_app():
     db = SQLAlchemy(app)
     db.init_app(app)
     migrate = Migrate(app, db)
-
-    engine = create_engine(os.environ['SQLALCHEMY_DATABASE_URI'])
-    session = Session(engine)
-
     db.create_all()
 
     class TodoList(db.Model):
@@ -35,7 +33,7 @@ def create_app():
         id = Column(Integer, primary_key=True)
         description = Column(String(), nullable=False)
         completed = Column(Boolean, nullable=False, default=False)
-        list_id = Column(Integer, ForeignKey('todolists.id'), nullable=False)
+        list_id = Column(Integer, ForeignKey('todolists.id'), nullable=True)
 
         def __repr__(self):
             return f'<Todo {self.id} {self.description}, list {self.list_id}>'
@@ -45,55 +43,55 @@ def create_app():
     def index():
       return render_template('index.html', data = Todo.query.all())
 
-    #create
-    @app.route('/todos/create', methods=['POST'])
-    def create_todo():
-        error = False
-        body = {}
-        try:
-            description = request.get_json()['description']
-            todo = Todo(description=description, completed=False)
-            session.add(todo)
-            session.commit()
-            body['id'] = todo.id
-            body['completed'] = todo.completed
-            body['description'] = todo.description
-        except:
-            error = True
-            session.rollback()
-            print(sys.exc_info())
-        finally:
-            session.close()
-        if error:
-            abort (400)
-        else:
-            return jsonify(body)
+    # #create
+    # @app.route('/todos/create', methods=['POST'])
+    # def create_todo():
+    #     error = False
+    #     body = {}
+    #     try:
+    #         description = request.get_json()['description']
+    #         todo = Todo(description=description, completed=False)
+    #         session.add(todo)
+    #         session.commit()
+    #         body['id'] = todo.id
+    #         body['completed'] = todo.completed
+    #         body['description'] = todo.description
+    #     except:
+    #         error = True
+    #         session.rollback()
+    #         print(sys.exc_info())
+    #     finally:
+    #         session.close()
+    #     if error:
+    #         abort (400)
+    #     else:
+    #         return jsonify(body)
 
-    @app.route('/todos/<todo_id>', methods=['DELETE'])
-    def delete_todo(todo_id):
-        try:
-            Todo.query.filter_by(id=todo_id).delete()
-            session.commit()                
-        except:
-                session.rollback()
-        finally:
-                session.close()
-        return jsonify({ 'success': True })
+    # @app.route('/todos/<todo_id>', methods=['DELETE'])
+    # def delete_todo(todo_id):
+    #     try:
+    #         Todo.query.filter_by(id=todo_id).delete()
+    #         session.commit()                
+    #     except:
+    #             session.rollback()
+    #     finally:
+    #             session.close()
+    #     return jsonify({ 'success': True })
     
-    #set complete
-    @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
-    def set_completed_todo(todo_id):
-        try:
-            completed = request.get_json()['completed']
-            print('completed', completed)
-            todo = Todo.query.get(todo_id)
-            todo.completed = completed
-            session.commit()
-        except:
-            session.rollback()
-        finally:
-            session.close()
-        return redirect(url_for('index'))
+    # #set complete
+    # @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+    # def set_completed_todo(todo_id):
+    #     try:
+    #         completed = request.get_json()['completed']
+    #         print('completed', completed)
+    #         todo = Todo.query.get(todo_id)
+    #         todo.completed = completed
+    #         session.commit()
+    #     except:
+    #         session.rollback()
+    #     finally:
+    #         session.close()
+    #     return redirect(url_for('index'))
 
 
     return app
